4ef04f0d57c97fd42821c10faa3a3236b613162b
Yasitha Herath <naveendray@gmail.com>
2024-03-30 23:10:10 +0530

    terraform to create RDS

---
+++ terraform/database/postgres_rds.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+variable "db_password" {
+  description = "Password for the database"
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier                = "wireapps"
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "postgres"
+  engine_version            = "16.1"
+  instance_class            = "db.t3.micro"
+  username                  = "admin"
+  password                  = var.db_password
+  publicly_accessible       = false
+  skip_final_snapshot       = true
+  apply_immediately         = true
+
+  tags = {
+    Name = "wireapps"
+  }
+}
+
